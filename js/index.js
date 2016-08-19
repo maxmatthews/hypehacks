db56505cea094d5bc4c3520a08ad1e0561d5e50d
TristanWiley <tristan@tristanwiley.com>
2016-08-19 00:16:01 -0400

    Dinosaurs, Devpost, oh my!

--- js/index.js
+++ js/index.js
@@ -1,40 +1,44 @@
 (function() {
-	var scrollSpeed = 400
+    $('.dinosaur').raptorize();
 
-	//Waypoints
-	$("section").each(function() {
-		var $this = $(this)
-		$this.waypoint({
-			handler: function() {
-				clearActiveNavs()
-				var id = $this.attr("id")
-				$("#nav-links li a[href='#" + id + "']").parent().attr("class", "active")
-			}
-		})
-	})
-	
-	$("header").waypoint({
-		handler: function() {
-			clearActiveNavs()
-		}
-	})
+    var scrollSpeed = 400
 
-	//Navbar click handlers
-	$("#nav-logo > a").click(function(e) {
-		e.preventDefault()
-		$(window).scrollTo(0, scrollSpeed)
-	})
+    //Waypoints
+    $("section").each(function() {
+        var $this = $(this)
+        $this.waypoint({
+            handler: function() {
+                clearActiveNavs()
+                var id = $this.attr("id")
+                $("#nav-links li a[href='#" + id + "']").parent().attr("class", "active")
+            }
+        })
+    })
 
-	$("#nav-links > li a").click(function(e) {
-		e.preventDefault()
-		var target = $(this).attr("href")
-		$(window).scrollTo($(target), scrollSpeed)
-	})
+    $("header").waypoint({
+        handler: function() {
+            clearActiveNavs()
+        }
+    })
 
-	//Utils
-	function clearActiveNavs() {
-		$("#nav-links li").each(function() {
-			$(this).attr("class", "")
-		})
-	}
+    //Navbar click handlers
+    $("#nav-logo > a").click(function(e) {
+        e.preventDefault()
+        $(window).scrollTo(0, scrollSpeed)
+    })
+
+    $("#nav-links > li a").click(function(e) {
+        e.preventDefault()
+        var target = $(this).attr("href")
+        $(window).scrollTo($(target), scrollSpeed)
+    })
+
+    //Utils
+    function clearActiveNavs() {
+        $("#nav-links li").each(function() {
+            $(this).attr("class", "")
+        })
+    }
+
+
 })()
