5f1e09d6f17d6a478f32d9b079dea225d47305d4
Arthelon <hsing.daniel@gmail.com>
2016-08-24 16:05:49 +0800

    added webpack

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+	entry: "./js/index",
+	output: {
+		path: "build/",
+		filename: "bundle.js",
+		publicPath: "/build/"
+	},
+	module: {
+		loaders: [
+			{test: /\.(svg|mp3|ogg|png|jpg)/, loaders: ["url", "file"]},
+			{test: /\.css/, loaders: ["style", "css"]}
+		]
+	},
+	externals: {
+		jquery: "jQuery"
+	},
+	resolve: {
+		extensions: [".js", ".css"]
+	},
+	devServer: {
+		hot: true
+	}
+}
